9c3815c76acf1b74648bb6008c54dddd12845e4d
Rimu Eirnarn <Rimuru720@proton.me>
2025-09-21 20:25:49 +0700

    optimized fatass runner

--- lymia/runner.py
+++ lymia/runner.py
@@ -1,4 +1,5 @@
 """Generic runners"""
+
 # pylint: disable=no-member,no-name-in-module
 
 import curses
@@ -16,20 +17,129 @@ from .utils import hide_system
 Ps = ParamSpec("Ps")
 DEBUG = True
 
-def wait(sizes: tuple[int, int], minsize: tuple[int, int]):
+
+def _oob_check(sizes: tuple[int, int], minsize: tuple[int, int]):
     """Check whether current sizes is enough for minsize"""
     return not (sizes[0] < minsize[0] or sizes[1] < minsize[1])
 
-def _oob_runner(render, fps: int, minsize: tuple[int, int]):
+
+def _oob_runner(render: curses.window, fps: int, minsize: tuple[int, int]):
     sizes = f"{minsize[0]}x{minsize[1]}"
     with hide_system(render):
         while True:
-            size: tuple[int, int] = get_terminal_size()[::-1] # type: ignore
+            size: tuple[int, int] = get_terminal_size()[::-1]  # type: ignore
             csize = f"{size[0]}x{size[1]}"
-            print(f"Cannot go below {sizes} ({csize})", end='\r')
-            if wait(size, minsize):
+            print(f"Cannot go below {sizes} ({csize})", end="\r")
+            if _oob_check(size, minsize):
                 return
             sleep(1 / fps)
+
+
+def check_resize(
+    render: curses.window,
+    scene: Scene,
+    sizes: tuple[int, int],
+    minsize: tuple[int, int],
+):
+    """Check scene resize"""
+    new_size = render.getmaxyx()
+    if sizes != new_size and scene.auto_resize:
+        scene.init(render)
+        sizes = new_size
+        minsize = max(scene.minimal_size, minsize)
+        if sizes[0] < minsize[0] or sizes[1] < minsize[1]:
+            _oob_runner(render, scene.render_fps, minsize)
+        return new_size
+    return sizes
+
+
+def wait(render_fps: int, start: float):
+    """Wait for FPS"""
+    if render_fps > 1:  # why do we add this anyway?
+        wend = perf_counter()
+        wdt = wend - start
+        frametime = 1 / render_fps
+        remaining = frametime - wdt
+        if remaining > 0:
+            sleep(remaining)
+
+
+def on_back(ret: ReturnType | None, stack: list[Scene], current: Scene):
+    """On back event"""
+    if ret in (ReturnType.BACK, ReturnType.ERR_BACK):
+        stack.pop()
+        current.on_unmount()
+        return True
+    return False
+
+
+def screen_update():
+    """Update screne"""
+    curses.panel.update_panels()
+    curses.doupdate()
+
+
+def report_fps(scene: Scene, fc: int, end: float, start: float):
+    """Report current FPS to current scene"""
+    if end - start >= 1.0:
+        scene.fps = fc / (end - start)
+        fc = 0
+        start = end
+    return fc, start
+
+
+def draw(scene: Scene, delta: float, start: float):
+    """Draw current scene"""
+    if scene.animator:
+        scene.animator.deltatime = delta
+    ret = scene.draw()
+    screen_update()
+    wait(scene.render_fps, start)
+    return ret
+
+
+def on_exit(ret: ReturnType, stack: list[Scene]):
+    """On exit"""
+    if ret == ReturnType.EXIT:
+        for scene in stack:
+            scene.on_unmount()
+        stack.clear()
+        return True
+    return False
+
+
+def on_push(result: SceneResult, screen: curses.window, stack: list[Scene]):
+    """On push"""
+    if isinstance(result, SceneResult):
+        scene = result.scene
+        scene.init(result.target or screen)
+        stack.append(scene)
+        return result.target or screen
+    return False
+
+
+def process_scene_result(
+    result: ReturnType | SceneResult,
+    stack: list[Scene],
+    current: Scene,
+    screen: curses.window,
+    root_screen: curses.window,
+):
+    """Process the result of handle_key and update stack/render as needed."""
+    if result == ReturnType.RETURN_TO_MAIN:
+        while len(stack) != 1:
+            scene = stack.pop()
+            scene.on_unmount()
+        return screen
+    if on_exit(result, stack):  # type: ignore
+        return False
+    if on_back(result, stack, current):  # type: ignore
+        return screen
+    pushed = on_push(result, root_screen, stack)  # type: ignore
+    if pushed:
+        return pushed
+    return None
+
 
 def runner(stdscr: curses.window, root: Scene, env: Theme | None = None):
     """Run the whole scheme"""
@@ -42,92 +152,41 @@ def runner(stdscr: curses.window, root: Scene, env: Theme | None = None):
     start = end = 0
     frame_count = 0
     window_start = perf_counter()
-    remaining = 0
-    wdt = 0
-    wend = 0
 
     if env:
         env.apply()
 
     while stack:
         start = perf_counter()
+        current = stack[-1]
 
-        comp = stack[-1]
-
-        if comp.should_clear:
+        if current.should_clear:
             render.erase()
 
-        new_size = render.getmaxyx()
-        if sizes != new_size and comp.auto_resize:
-            comp.init(render)
-            sizes = new_size
+        sizes = check_resize(render, current, sizes, root_minsize)
+        ret = draw(current, delta, start)
 
-            minsize = max(comp.minimal_size, root_minsize)
-            if sizes[0] < minsize[0] or sizes[1] < minsize[1]:
-                _oob_runner(render, comp.render_fps, minsize)
-
-        if comp.animator:
-            comp.animator.deltatime = delta
-        ret = comp.draw()
-        curses.panel.update_panels()
-        curses.doupdate()
-        if comp.render_fps > 1: # why do we add this anyway?
-            wend = perf_counter()
-            wdt = wend - start
-            frametime = 1 / comp.render_fps
-            remaining = frametime - wdt
-            if remaining > 0:
-                sleep(remaining)
-            # comp.fps = 1 / delta
-
-        # curses.panel.update_panels()
-        if ret in (ReturnType.BACK, ReturnType.ERR_BACK):
-            stack.pop()
-            comp.on_unmount()
+        if on_back(ret, stack, current):
             render = stdscr
             continue
 
         key = render.getch()
-        result = comp.handle_key(key)
-        curses.panel.update_panels()
-        curses.doupdate()
-
-        # if DEBUG:
-            # status.set(f"{comp!r}: {result!r}")
-            # comp.show_status(stdscr)
-
-        if result == ReturnType.RETURN_TO_MAIN:
-            render = stdscr
-            while len(stack) != 1:
-                scene = stack.pop()
-                scene.on_unmount()
+        result = current.handle_key(key)
+        screen_update()
 
         # comp.handle_key() may mutate display
         end = perf_counter()
         delta = end - start
         frame_count += 1
-        if end - window_start >= 1.0:
-            comp.fps = frame_count / (end - window_start)
-            frame_count = 0
-            window_start = end
+        frame_count, window_start = report_fps(current, frame_count, end, window_start)
 
-        if result == ReturnType.EXIT:
-            for scene in stack:
-                scene.on_unmount()
-            stack.clear()
+        new_render = process_scene_result(result, stack, current, render, stdscr)
+        if new_render is False:
             break
-
-        if result in (ReturnType.BACK, ReturnType.ERR_BACK):
-            stack.pop()
-            comp.on_unmount()
-            render = stdscr
+        if new_render is None:
             continue
+        render = new_render
 
-        if isinstance(result, SceneResult):
-            scene = result.scene
-            render = result.target or stdscr
-            scene.init(render)
-            stack.append(scene)
 
 def bootstrap(fn: Callable[Ps, tuple[Scene, Theme | None]]):
     """Run the app, must be used as decorator like:
@@ -136,16 +195,20 @@ def bootstrap(fn: Callable[Ps, tuple[Scene, Theme | None]]):
     def init():
         ...
     """
+
     @wraps(fn)
     def inner(*args, **kwargs):
         return curses.wrapper(runner, *fn(*args, **kwargs))
+
     return inner
+
 
 def debug(fn: Callable[Ps, tuple[Scene, Theme | None]]):
     """Enable debug mode"""
-    global DEBUG # pylint: disable=global-statement
+    global DEBUG  # pylint: disable=global-statement
     DEBUG = True
     return fn
+
 
 def run(_fn: Callable[Ps, tuple[Scene, Theme | None]], *args, **kwargs):
     """Run main function, the structure must be similiar of `@bootstrap` target function."""
